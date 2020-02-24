865900cc0f5f66c8fffa123c418f478fabdac282
AT-hash <58159718+AT-hash@users.noreply.github.com>
2020-02-24 09:48:31 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,86 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>教育支援サイト</title>
+    <link rel = "stylesheet" , href = "kihon.css">
+</head>
+<body>
+<div id="page">
+    <header>
+        <div id="header_inner">
+            <div id="header_logo">
+            </div>
+            <div id="header_contact">
+                <a href="#" id="header_contact_inner">
+                    <p>お問い合わせ</p>
+                </a>
+            </div>
+            <div id="header_text">
+                <p><br></p>
+            </div>
+        </div>
+    </header>
+    <div id="main_image">
+        <div id="main_image_inner">
+            <h1>国語力を伸ばす</h1>
+        </div>
+    </div>
+    <main>
+        <section id="section01">
+            <div class="scroll">
+                <h2>未来に向けて</h2>
+            </div>
+            <nav>
+                <ul id="main_nav">
+                    <li><a href="#div01">1.読解力を<br>伸ばす</a></li>
+                    <li><a href="#div03">3.倫理観を<br>良くする</a></li>
+                    <li><a href="#div02">2.価値観を<br>広げる</a></li>
+                </ul>
+            </nav>
+            <div class="contents" id="div01">
+                <div class="left" id="photo01">
+                </div>
+                <div class="right text">
+                    <h4>1.読解力を伸ばす</h4>
+                    <p>昨今の小中高生の読解力は例年に比べて７５％と低下しています。</p>
+                </div>
+            </div>
+            <div class="contents" id="div02">
+                <div class="left text">
+                    <h4>2.価値観を広げる</h4>
+                    <p>小中高生にとってはまだ世界の様々な価値観を知る経験がどうしても少ないもの</p>
+                </div>
+                <div class="right" id="photo02">
+                </div>
+            </div>
+            <div class="contents" id="div03">
+                <div class="left" id="photo03">
+                </div>
+                <div class="right text">
+                    <h4>3.倫理観を良くする</h4>
+                    <p>自信の無さを</p>
+                </div>
+            </div>
+            <div class="main_contact">
+                <div class="contact_left main_contact_inner">
+                    <div class="contact_left_inner">
+                        <p><br>
+                            <span></span></p>
+                    </div>
+                </div>
+                <div class="contact_right main_contact_inner">
+                    <a href="#" class="contact_right_inner">
+                        <p>お問い合わせボタン</p>
+                    </a>
+                </div>
+            </div>
+        </section>
+    </main>
+    <footer>
+        <p>&copy; 国語力特化型</p>
+    </footer>
+</div>
+</body>
+</html>
